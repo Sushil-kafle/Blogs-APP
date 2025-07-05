d15482d692329acf8382694fac086d770adb34f8
Sushil-kafle <susil.kafle123@gmail.com>
2025-07-05 17:03:02 +0000

    feat: final last fix check lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { CssBaseline, ThemeProvider, createTheme, Box, Typography, Button } from
 const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const theme = createTheme();
+// changed code
 
 function App() {
   return (
